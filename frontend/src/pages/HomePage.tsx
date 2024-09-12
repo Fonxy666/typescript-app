397a1cd5d8b11fae1d4510e3edb0aaf623d93a21
fonxy6 <viktor_6@windowslive.com>
2024-09-12 12:35:54 +0200

    feature: imported react router dom, created homepage skeleton, and moved the test cases to :"/test" endpoint

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React, { FormEvent, useRef, useState } from 'react';
+
+const HomePage: React.FC = () => {
+   
+
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+};
+
+export default HomePage;
