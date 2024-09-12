780a58b9d7342a9de48cd67beb650b1c2734ed1c
fonxy6 <viktor_6@windowslive.com>
2024-09-12 13:34:43 +0200

    feature: started to style frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import './App.css';
 import TestPage from "./pages/TestPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import "./styles/index.scss";
 
 function App() {
     const router = createBrowserRouter([
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React, { FormEvent, useRef, useState } from 'react';
+import Navbar from '../components/Navbar';
+import "../styles/index.scss";
 
 const HomePage: React.FC = () => {
    
-
     return (
         <div>
+            <Navbar/>
             Hello World!
         </div>
     );
