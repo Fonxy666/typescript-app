a5ecfd0c943ab1a07b13709e59be55f2c424daa0
fonxy6 <viktor_6@windowslive.com>
2024-09-13 11:59:49 +0200

    feature: footer completed

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { HeroSection } from '../components/HeroSection';
 import { ImproveSkills } from '../components/ImproveSkills';
 import { QuoteSection } from '../components/QuoteSection';
 import { TopChiefs } from '../components/TopChiefs';
+import { Footer } from '../components/Footer';
 
 const HomePage: React.FC = () => {
    
@@ -17,6 +18,7 @@ const HomePage: React.FC = () => {
                 <QuoteSection />
                 <TopChiefs />
             </div>
+            <Footer />
         </div>
     );
 };
